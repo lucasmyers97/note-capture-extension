c808b36471364c5dbd74eb389d6315f6b6262916
Lucas Myers <lucasmyers97@gmail.com>
2026-04-23 22:52:19 -0500

    Fixed issue with empty notes
    Based on LiquidJS, `if highlight_note` would be true even if the string
    was just "", so you would get a new line with just `Note:`. Fixed the
    default string to check whether `highlight_note` is just "", instead of
    checking for its existence.

--- src/add-on/default_options.ts
+++ src/add-on/default_options.ts
@@ -4,7 +4,7 @@ const frontmatter_default = `#+title: {{ title }}
 #+date: {{ date }}{% if url %}
 #+url: {{ url }}{% endif %}`;
 const highlight_default = `* {{ highlight_text | replace: '\\n' : ' ' | truncate: 200 , "" | split: " " | pop | join: " " }}
-  - {{ highlight_text | replace: '\\n' , ' '  }}{% if highlight_note %}
+  - {{ highlight_text | replace: '\\n' , ' '  }}{% if highlight_note != "" %}
   - Note: {{ highlight_note | replace: '\\n' , ' ' }}{% endif %}`
 
 export const option_defaults = {
